9cd0253bf5722bfe546a8685cc1fe59758cd25d1
Otto White <whiteotto4@gmail.com>
2022-11-14 16:01:46 +0000

    refactor: unify 4 space indentation style

--- batting_drill.py
+++ batting_drill.py
@@ -2,10 +2,10 @@ from cover_drive_judge import CoverDriveJudge
 import sys
 
 def main(input_filename):
-  with CoverDriveJudge(input_filename) as judge:
-    judge.process_and_write_video()
+    with CoverDriveJudge(input_filename) as judge:
+        judge.process_and_write_video()
 
 
 if __name__ == "__main__":
-  main(sys.argv[1])
+    main(sys.argv[1])
 
