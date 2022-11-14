47b08ae92574b9d7aedb18b2535400f6088a18a4
r-ushil <rushil.patel791@gmail.com>
2022-11-14 12:53:06 +0000

    feat: checks vertical alignment between 3 joints given a tolerance

--- batting-drill.py
+++ batting-drill.py
@@ -76,3 +76,11 @@ def calculate_angle(a, b, c):
         angle = 360-angle
         
     return angle
+
+# Checks 3 joints are vertically aligned, with a tolerance on acceptable angle (in degrees)
+def check_vertical_alignment(shoulder, knee, foot, tolerance):
+    vertical_alignment = calculate_angle(shoulder, knee, foot)
+    if vertical_alignment > (180 - tolerance) and vertical_alignment < (180 + tolerance):
+        return False
+    else:
+        return True
