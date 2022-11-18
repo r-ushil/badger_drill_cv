89d0f536613b73204487a747b6002af16a9fc3ba
Oli Callaghan <me@olicallaghan.com>
2022-11-18 16:02:00 +0000

    fix: specify service account email and token on generate_signed_url request

--- storage.py
+++ storage.py
@@ -1,23 +1,11 @@
-from datetime import timedelta
-from os import environ
 from google import auth
 from google.cloud import storage
-
-import google
-from google.auth.transport import requests
-from google.auth import compute_engine
+from datetime import timedelta
+from os import environ
 
 credentials, project = auth.default()
 
-auth_request = requests.Request()
-credentials, project = google.auth.default()
-storage_client = storage.Client(project, credentials)
-
-signing_credentials = compute_engine.IDTokenCredentials(
-	auth_request,
-	"",
-	service_account_email=credentials.service_account_email,
-)
+storage_client = storage.Client()
 
 def _get_bucket() -> storage.Bucket:
 	bucket_name = environ.get("BUCKET_NAME")
@@ -32,8 +20,15 @@ def get_object_signed_url(obj_name: str) -> str:
 	bucket_obj = _get_object(obj_name)
 	bucket_obj_expiry = timedelta(minutes=5)
 
+	from google.auth.transport import requests
+	request = requests.Request()
+	credentials.refresh(request)
+
+	if not hasattr(credentials, "service_account_email"):
+		print("Missing service_account_email")
+
 	return bucket_obj.generate_signed_url(
 		expiration=bucket_obj_expiry,
-		credentials=signing_credentials,
-		version="v4"
+		service_account_email=credentials.service_account_email,
+		access_token=credentials.token,
 	)
