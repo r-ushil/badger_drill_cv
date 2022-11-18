23d0678411163304cccf5638654f437391f19faa
Oli Callaghan <me@olicallaghan.com>
2022-11-18 15:27:44 +0000

    fix: use v4 signed urls

--- storage.py
+++ storage.py
@@ -7,27 +7,28 @@ from google.cloud import storage
 
 credentials, project = auth.default()
 signing_credentials = compute_engine.IDTokenCredentials(
-    requests.Request(),
-    "",
-    service_account_email=credentials.service_account_email
+	requests.Request(),
+	"",
+	service_account_email=credentials.service_account_email
 )
 
 storage_client = storage.Client(credentials=signing_credentials)
 
 def _get_bucket() -> storage.Bucket:
-    bucket_name = environ.get("BUCKET_NAME")
+	bucket_name = environ.get("BUCKET_NAME")
 
-    return storage_client.bucket(bucket_name)
+	return storage_client.bucket(bucket_name)
 
 def _get_object(obj_name: str) -> storage.Blob:
-    bucket = _get_bucket()
-    return bucket.blob(obj_name)
+	bucket = _get_bucket()
+	return bucket.blob(obj_name)
 
 def get_object_signed_url(obj_name: str) -> str:
-    bucket_obj = _get_object(obj_name)
-    bucket_obj_expiry = timedelta(minutes=5)
+	bucket_obj = _get_object(obj_name)
+	bucket_obj_expiry = timedelta(minutes=5)
 
-    return bucket_obj.generate_signed_url(
-        expiration=bucket_obj_expiry,
-        credentials=signing_credentials,
-    )
+	return bucket_obj.generate_signed_url(
+		expiration=bucket_obj_expiry,
+		credentials=signing_credentials,
+		version="v4"
+	)
