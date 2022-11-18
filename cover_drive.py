3ef3c439f1eaffa3f35848503da85d7e12ef1a1d
Oli Callaghan <me@olicallaghan.com>
2022-11-18 12:43:12 +0000

    refactor: use Flask blueprints to modularise per-drill

--- cover_drive.py
+++ cover_drive.py
@@ -5,7 +5,6 @@ def main(input_filename):
     with CoverDriveJudge(input_filename) as judge:
         (averageScore, advice1, advice2) = judge.process_and_write_video()
 
-
 if __name__ == "__main__":
     main(sys.argv[1])
 
