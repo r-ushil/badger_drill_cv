6735839eefd0b49e387d1409df4ce681d09e4ca6
Otto White <whiteotto4@gmail.com>
2022-11-28 16:37:02 +0000

    feat: implement plane and intersects with point function

---
+++ plane.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+class Plane:
+	def __init__(self, p1, p2, p3):
+		assert p1.shape == (3, )
+		assert p2.shape == (3, )
+		assert p3.shape == (3, )
+
+		# Get two vectors along the plane
+		v1 = p2 - p1
+		v2 = p3 - p1
+
+		# Calculate normal vector d, store as plane representation:
+		# ax + by + cz = d
+		self.n = np.cross(v1, v2)
+		self.d = np.dot(self.n, p1)
+
+	def intersects_with_point(self, p):
+		assert p.shape == (3, )
+
+		return np.dot(self.n, p) == self.d
+
+def main():
+	p1 = np.array([1, 2, 3])
+	p2 = np.array([4, 9, 6])
+	p3 = np.array([4, 8, 21])
+
+	plane = Plane(p1, p2, p3)
+
+	p4 = np.array([5, 6, 4])
+	print(plane.intersects_with_point(p4))
+
+if __name__ == "__main__":
+	main()
