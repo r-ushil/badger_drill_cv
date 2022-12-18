f0c4932a1f50f5b7c02dba9920899685cfe6f112
Otto White <whiteotto4@gmail.com>
2022-12-18 18:14:09 +0000

    feat: implement helper to rotate and scale one vector onto another

--- line.py
+++ line.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 class Line:
 	def __init__(self, p1, p2):
 		assert p1.shape == (3, )
@@ -5,4 +7,32 @@ class Line:
 	
 		self.d = p2 - p1
 		self.v0 = p1
+
+	
+	# Finds the vector that rotates and scales 
+	# v1 onto v2
+	@staticmethod
+	def find_rotation_matrix(v1, v2):
+		# https://math.stackexchange.com/questions/180418/calculate-rotation-matrix-to-align-vector-a-to-vector-b-in-3d
+		assert v1.shape == (3, 1)
+		assert v2.shape == (3, 1)
+
+		v1norm = np.linalg.norm(v1)
+		v2norm = np.linalg.norm(v2)
+
+		v1unit = v1 / v1norm
+		v2unit = v2 / v2norm
+
+		v = np.cross(v1unit, v2unit, axis=0)
+		c = np.dot(v1unit.reshape(3,), v2unit.reshape(3,))
+		I = np.identity(3)
+		s = np.linalg.norm(v)
+		k = v2norm / v1norm
+		vx = np.array([
+			[0, 	-v[2],	v[1]],
+			[v[2], 	0,		-v[0]],
+			[-v[1], v[0], 	0],
+		])
+
+		return k * (I + vx + np.dot(vx, vx) * ((1 - c) / np.square(s)))
 	
