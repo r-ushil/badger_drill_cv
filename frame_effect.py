4bdb2611e6e1833faa2b144dc2d2c9a3acbd246f
Otto White <whiteotto4@gmail.com>
2022-12-27 22:45:47 +0000

    feat: add mandatory primary label and display label to FrameEffect

--- frame_effect.py
+++ frame_effect.py
@@ -7,15 +7,18 @@ class FrameEffectType(Enum):
 
 class FrameEffect:
 	def __init__(self, 
-		frame_effect_type, 
+		frame_effect_type=None, 
+		primary_label=None, 
 		points_multiple=None, 
 		point_single=None, 
 		colour=None,
-		label=None,
+		display_label=None,
 		show_label=None
 		):
 		assert frame_effect_type is not None
+		assert primary_label is not None
 		self.frame_effect_type = frame_effect_type
+		self.primary_label = primary_label
 
 		match frame_effect_type:
 			case FrameEffectType.POINTS_MULTIPLE:
@@ -29,12 +32,12 @@ class FrameEffect:
 				assert point_single is not None
 				assert point_single.shape == (3, 1)
 				assert colour is not None
-				assert label is not None
+				assert display_label is not None
 				assert show_label is not None
 
 				self.point_single = point_single
 				self.colour = colour
-				self.label = label
+				self.display_label = display_label
 				self.show_label = show_label
 
 
