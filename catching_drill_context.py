cb6a6d62aac6239e870f238149a6004307958a81
Otto White <whiteotto4@gmail.com>
2022-12-28 22:40:04 +0000

    refactor: move cam_pose_estimator onto drill context, remove from frame context

--- catching_drill_context.py
+++ catching_drill_context.py
@@ -1,8 +1,12 @@
 class CatchingDrillContext():
 	__ball_positions: list
 
-	def __init__(self) -> None:
+	def __init__(self, cam_pose_estimator) -> None:
 		self.__ball_positions = []
+		self.__cam_pose_estimator = cam_pose_estimator
+
+	def get_cam_pose_estimator(self):
+		return self.__cam_pose_estimator
 
 	def register_ball_position(self, ball_position):
 		self.__ball_positions.append(ball_position)
