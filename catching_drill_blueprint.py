c54cac77b6b919fd1dc958fea75a6f4c6dfa9a5c
Oli Callaghan <me@olicallaghan.com>
2023-01-11 13:56:15 +0000

    feat: define catching drill endpoint and specify no output

---
+++ catching_drill_blueprint.py
@@ -0,0 +1,35 @@
+from math import isnan
+from flask import Blueprint, jsonify, request
+
+from catching_judge import CatchingJudge
+
+from storage import get_object_signed_url
+
+catching_drill_blueprint = Blueprint('catching_drill', __name__)
+
+@catching_drill_blueprint.route("/catching-drill", methods=["POST"])
+def process_catching_drill_video():
+	req_json = request.json
+	obj_name = req_json.get('video_object_name', None)
+
+	if type(obj_name) is not str:
+		for key, value in request.args.items():
+			print(key, value)
+		return 'Missing video_object_name', 400
+
+	obj_signed_url = get_object_signed_url(obj_name)
+
+	with CatchingJudge(obj_signed_url, no_output=True) as judge:
+		result = judge.process_and_write_video()
+
+		if result.error is not None:
+			return jsonify(
+				error_code=result.get_error_code(),
+				error_message=result.get_error_message(),
+			), 400
+
+		return jsonify(
+            ball_speed=result.speed,
+            ball_max_height=result.max_height,
+            ball_angle=result.angle,
+        )
