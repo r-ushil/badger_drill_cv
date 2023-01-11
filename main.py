2a470aa113b1f08cbed94d7f761dd92c7fb04d78
Oli Callaghan <me@olicallaghan.com>
2023-01-11 13:56:26 +0000

    feat: register catching drill blueprint

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from os import environ
 from flask import Flask
 
+from catching_drill_blueprint import catching_drill_blueprint
 from cover_drive_blueprint import cover_drive_blueprint
 
 app = Flask(__name__)
 
+app.register_blueprint(catching_drill_blueprint)
 app.register_blueprint(cover_drive_blueprint)
 
 if __name__ == "__main__":
